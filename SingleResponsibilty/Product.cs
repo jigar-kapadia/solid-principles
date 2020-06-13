0777009d6ac89fe0b7617614b84986538c9de7d9
jigar.kapadia <jigar.kapadia@learningmate.com>
2020-06-13 18:16:16 +0530

    Initial Commit

---
+++ SingleResponsibilty/Product.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilty
+{
+    public class Product
+    {
+        public Product()
+        {
+            Products = new List<Product>();
+            //Products.Add(new Product() { Id = 1, Name = "Iphone 6", Price = 199, Brand = "Apple", Type = "Mobile", Color = "Black" });
+            //Products.Add(new Product() { Id = 2, Name = "Iphone 7", Price = 229, Brand = "Apple", Type = "Mobile", Color = "Silver" });
+            //Products.Add(new Product() { Id = 3, Name = "Oneplus 8", Price = 200, Brand = "Oneplus", Type = "Mobile", Color = "Black" });
+            //Products.Add(new Product() { Id = 4, Name = "Oneplus 7t", Price = 179, Brand = "Oneplus", Type = "Mobile", Color = "Black" });
+            //Products.Add(new Product() { Id = 5, Name = "Ipad 3", Price = 179, Brand = "Apple", Type = "Tablet", Color = "White" });
+            //Products.Add(new Product() { Id = 6, Name = "Redmi Note 9", Price = 199, Brand = "MI", Type = "Mobile", Color = "Silver" });
+            //Products.Add(new Product() { Id = 7, Name = "Samsung Galaxy Note 10", Price = 159, Brand = "Samsung", Type = "Mobile", Color = "White" });
+            //Products.Add(new Product() { Id = 8, Name = "Samsung Galaxy S20 Ultra", Price = 219, Brand = "Samsung", Type = "Mobile", Color = "Blue" });
+            //Products.Add(new Product() { Id = 9, Name = "Samsung Galaxy Tab S6", Price = 199, Brand = "Samsung", Type = "Tablet", Color = "Blue" });
+            //Products.Add(new Product() { Id = 10, Name = "Lenovo Tab M10 HD", Price = 105, Brand = "Lenovo", Type = "Tablet", Color = "Silver" });
+            //Products.Add(new Product() { Id = 11, Name = "Redmi K20 Pro", Price = 109, Brand = "MI", Type = "Mobile", Color = "Black" });
+            //Products.Add(new Product() { Id = 12, Name = "Nokia 6.2", Price = 200, Brand = "Nokia", Type = "Mobile", Color = "White" });
+            //Products.Add(new Product() { Id = 13, Name = "Nokia 4.2", Price = 160, Brand = "Nokia", Type = "Mobile", Color = "Blue" });
+            //Products.Add(new Product() { Id = 13, Name = "Nokia 7.2", Price = 175, Brand = "Nokia", Type = "Mobile", Color = "Black" });
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+
+        private List<Product> Products { get; set; }
+
+        public List<Product> GetProducts()
+        {
+            //Products = new List<Product>();
+            Products.Add(new Product() { Id = 1, Name = "Iphone 6", Price = 199, Brand = "Apple", Type = "Mobile", Color = "Black" });
+            Products.Add(new Product() { Id = 2, Name = "Iphone 7", Price = 229, Brand = "Apple", Type = "Mobile", Color = "Silver" });
+            Products.Add(new Product() { Id = 3, Name = "Oneplus 8", Price = 200, Brand = "Oneplus", Type = "Mobile", Color = "Black" });
+            Products.Add(new Product() { Id = 4, Name = "Oneplus 7t", Price = 179, Brand = "Oneplus", Type = "Mobile", Color = "Black" });
+            Products.Add(new Product() { Id = 5, Name = "Ipad 3", Price = 179, Brand = "Apple", Type = "Tablet", Color = "White" });
+            Products.Add(new Product() { Id = 6, Name = "Redmi Note 9", Price = 199, Brand = "MI", Type = "Mobile", Color = "Silver" });
+            Products.Add(new Product() { Id = 7, Name = "Samsung Galaxy Note 10", Price = 159, Brand = "Samsung", Type = "Mobile", Color = "White" });
+            Products.Add(new Product() { Id = 8, Name = "Samsung Galaxy S20 Ultra", Price = 219, Brand = "Samsung", Type = "Mobile", Color = "Blue" });
+            Products.Add(new Product() { Id = 9, Name = "Samsung Galaxy Tab S6", Price = 199, Brand = "Samsung", Type = "Tablet", Color = "Blue" });
+            Products.Add(new Product() { Id = 10, Name = "Lenovo Tab M10 HD", Price = 105, Brand = "Lenovo", Type = "Tablet", Color = "Silver" });
+            Products.Add(new Product() { Id = 11, Name = "Redmi K20 Pro", Price = 109, Brand = "MI", Type = "Mobile", Color = "Black" });
+            Products.Add(new Product() { Id = 12, Name = "Nokia 6.2", Price = 200, Brand = "Nokia", Type = "Mobile", Color = "White" });
+            Products.Add(new Product() { Id = 13, Name = "Nokia 4.2", Price = 160, Brand = "Nokia", Type = "Mobile", Color = "Blue" });
+            Products.Add(new Product() { Id = 13, Name = "Nokia 7.2", Price = 175, Brand = "Nokia", Type = "Mobile", Color = "Black" });
+            return Products;
+        }
+    }
+}
---
+++ SingleResponsibilty/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilty
+{
+    
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> list, ISpecification<T> specification);
+    }
+
+    public interface ISpecification<T>
+    {
+        bool IsFulfilled(Product product);
+    }
+
+    public class ProductFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> list, ISpecification<Product> specification)
+        {
+           return list.Where(product => specification.IsFulfilled(product));
+        }
+    }
+
+    public class BrandSpecification : ISpecification<Product>
+    {
+        private string _brand;
+        public BrandSpecification(string brand)
+        {
+            this._brand = brand;
+        }
+        public bool IsFulfilled(Product product)
+        {
+            return product.Brand.ToLower() == this._brand.ToLower();
+        }
+    }
+
+    public class TypeSpecification : ISpecification<Product>
+    {
+        private readonly string _type;
+
+        public TypeSpecification(string type)
+        {
+            _type = type;
+        }
+        public bool IsFulfilled(Product product)
+        {
+            return product.Type.ToLower() == _type.ToLower();
+        }
+    }
+
+    public class ColorSpecification : ISpecification<Product>
+    {
+        private readonly string _color;
+        public ColorSpecification(string color)
+        {
+            _color = color;
+        }
+        public bool IsFulfilled(Product product)
+        {
+            return product.Color.ToLower() == _color.ToLower();
+        }
+    }
+
+    public class AndSpecification : ISpecification<Product>
+    {
+        private ISpecification<Product> _spec1, _spec2;
+        private ISpecification<Product>[] _specArray;
+        public AndSpecification(params ISpecification<Product>[] specs)
+        {
+            _specArray = specs;
+
+            //_spec1 = spec1;
+            //_spec2 = spec2;
+        }
+
+        public bool IsFulfilled(Product product)
+        {
+            if (_specArray.Length == 2)
+                return _specArray[0].IsFulfilled(product) && _specArray[1].IsFulfilled(product);
+            else if (_specArray.Length == 3)
+                return _specArray[0].IsFulfilled(product) && _specArray[1].IsFulfilled(product) &&
+                    _specArray[2].IsFulfilled(product);
+            else
+                return false;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var product = new Product();
+            var products = product.GetProducts();
+            var productFilter = new ProductFilter();
+
+            //All Apple
+            Console.WriteLine("All Apple Mobiles Available");
+            Console.WriteLine();
+            foreach (var item in productFilter.Filter(products, new BrandSpecification("Apple")))
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            //All Tablets
+            Console.WriteLine("All Tablets Available");
+            Console.WriteLine();
+            foreach (var item in productFilter.Filter(products, new TypeSpecification("Tablet")))
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            //All silver color
+            Console.WriteLine("All Silver Color Products Available");
+            Console.WriteLine();
+            foreach (var item in productFilter.Filter(products, new ColorSpecification("Silver")))
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            //All black mobile
+            Console.WriteLine("All Black Color Mobiles Available");
+            Console.WriteLine();
+            foreach (var item in productFilter.Filter(products, 
+                new AndSpecification(new TypeSpecification("mobile"), new ColorSpecification("black"))))
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+}

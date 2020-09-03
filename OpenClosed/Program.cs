5d70557aa4f58b0e3f17b4e5970d73f0961940a3
jigar.kapadia <jigar.kapadia@learningmate.com>
2020-09-03 21:56:20 +0530

    final backup

---
+++ OpenClosed/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosed
+{
+    public class Program
+    {
+        //Violating OCP
+        
+
+        //Without Violating OCP
+
+
+        public static void Main(string[] args)
+        {
+           Console.ReadLine();
+        }
+    }
+}
